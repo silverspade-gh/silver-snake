6feab2b58132020c97fb5d8a01334da16c0dccab
silverspade-gh <silver@bucketofdata.com>
2024-11-13 12:45:11 -0500

    Update and rename swimming_rocks.py to swimmingrocks.py
    Added an example of inheritance at line 29.

--- swimmingrocks.py
+++ swimmingrocks.py
@@ -1,8 +1,8 @@
-# swimming_rocks.py
+# swimmingrocks.py
 # For more information on the 'self' parameter,
 # refer to 9.3.4 in the docs: https://docs.python.org/3/tutorial/classes.html#method-objects
 
-class swimming_rock:
+class SwimmingRock:
     """A collection of rocks that can swim."""
     
     # This is a constructor.
@@ -11,26 +11,33 @@ class swimming_rock:
     def __init__(self, color='Gray', swim_stroke='Front crawl'):
         self.color = color
         self.swim_stroke = swim_stroke
-          
+
     # This is a procedure.
     # It can change the rock's color.
     # This couldn't be possible in the real world, but can be in Python!
     def change_color(self, new_color):
         self.color = new_color
-    
+
     # This is a function.
     # It's a procedure that returns a value.
-    def talk(self):
-    
-        # I wouldn't want my rock talking, so it returns an empty string!
+    def talk(self, say=''):
+        # I wouldn't want my rock talking.
+        # No matter what value is passed for the parameter say,
+        # The function always returns an empty string!
         return ''
 
+class TalkingSwimmingRock(SwimmingRock):
+
+    def talk(self, say=''):  # It's the same function name and signature, but...
+        return say  # It returns the variable say, not an empty string!
+        # This is known as function overriding, a type of polymorphism.
+
 # By including this common conditional, the code will only run
-# if the file is run as is (i.e., python swimming_rocks.py)
-# and not as an imported module (i.e., import swimming_rocks).       
+# if the file is run as is (i.e., python swimmingrocks.py)
+# and not as an imported module (i.e., import swimmingrocks).
 if __name__ == '__main__':
-    my_pet_rock = swimming_rock()
+    my_pet_rock = SwimmingRock()
     
-    # This is equivalent to swimming_rock.talk(my_pet_rock)
+    # This is equivalent to SwimmingRock.talk(my_pet_rock)
     # as documented in 9.3.4 on the 'self' parameter.
     print(my_pet_rock.talk())
